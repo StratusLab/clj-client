c8e7243c47adaedee8c9faf8beb9cc03fcac0c72
Charles Loomis <cal@sixsq.com>
2012-05-25 14:34:01 +0200

    update project file and eclipse files

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clojure-ini "0.0.1"]
-                 [necessary-evil "1.2.2"]])
+                 [necessary-evil "1.2.2"]
+                 [lein-eclipse "1.0.0"]])
+
 
 
