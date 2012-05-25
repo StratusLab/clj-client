e84f7213eef2324aa00ccdbf0df607a9f1895bb9
Charles Loomis <cal@sixsq.com>
2012-05-25 14:24:55 +0200

    leiningen skeleton added along with expected dependencies

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject cloud-client "0.1-SNAPSHOT"
+  :description "simple StratusLab cloud client in clojure"
+  :url "https://github.com/loomis/cloud-client"
+  :license {:name "Apache 2"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clojure-ini "0.0.1"]
+                 [necessary-evil "1.2.2"]])
+
+
---
+++ test/cloud_client/core_test.clj
@@ -0,0 +1,7 @@
+(ns cloud-client.core-test
+  (:use clojure.test
+        cloud-client.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
